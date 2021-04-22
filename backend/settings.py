1660b0c2384366a8f5edc25c197b5a5ab995dd2a
yoshinoseiji <sij.ysn25@gmail.com>
2021-04-23 05:16:51 +0900

    Update

--- backend/settings.py
+++ backend/settings.py
@@ -187,7 +187,7 @@ except ImportError:
     pass
 
 if not DEBUG:
-    SECRET_KEY = os.environ['SECRET_KEY']
+    # SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
     django_heroku.settings(locals())
 
