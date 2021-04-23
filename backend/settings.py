1e2bf820d20b86cbfb5178432654e3487497718c
yoshinoseiji <sij.ysn25@gmail.com>
2021-04-23 14:21:40 +0900

    Update ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'proshop-demo.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'tinder-clone3084.herokuapp.com']
 
 
 # Application definition
