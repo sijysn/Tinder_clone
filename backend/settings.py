85f05b81f5edc32a7962209507506ad0d36efda8
yoshinoseiji <sij.ysn25@gmail.com>
2021-04-23 09:16:21 +0900

    Update

--- backend/settings.py
+++ backend/settings.py
@@ -195,7 +195,7 @@ if not DEBUG:
     import django_heroku
     django_heroku.settings(locals())
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 LOGGING = {
     'version': 1,
