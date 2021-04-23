837c1e4a65d021cbe9e9232b68df96076e839a0e
yoshinoseiji <sij.ysn25@gmail.com>
2021-04-23 14:00:20 +0900

    Update static_root

--- backend/settings.py
+++ backend/settings.py
@@ -179,7 +179,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CORS_ALLOW_ALL_ORIGINS = True
 
